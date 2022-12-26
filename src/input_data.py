0b0872b4d36f2f3db64d3051d392e298b5ac222a
Anthony Wynne <a.wynne@svgc.co.uk>
2022-12-26 08:58:55 +0000

    added db class

--- src/input_data.py
+++ src/input_data.py
@@ -5,27 +5,11 @@ import logging
 wikipedia.set_lang("en")
 
 
-def get_wiki_summary(name="Monty Python", sentences=10) -> str:
-    """A wikipedia fetcher function
-
-    Args:
-        name (str, optional): The name of the page to fetch.
-            Defaults to "Monty Python".
-        sentences (int, optional): The number of sentences to fetch.
-            Defaults to the first 10
-            can be no greater than 10.
-
-    Returns:
-        str: The fetched summary of the wikipedia page
-    """
-    return wikipedia.summary(name, sentences=sentences)
-
-
 def get_random_wiki_page(pages=2) -> list[str]:
     """Fetch a random wikipedia page title. Defaults to 2 pages
     If a single page is returnd it is a string, if multiple pages
     are returned it is a list of strings. So we always return a
-    list with 2.
+    list with 2. And return the first element.
 
     Args:
         pages (int, optional): The number of pages titles.
@@ -34,33 +18,61 @@ def get_random_wiki_page(pages=2) -> list[str]:
         Returns:
             list[str]: The titles of the random pages
     """
-    return wikipedia.random(pages=pages)
+    return wikipedia.random(pages=pages)[0]
 
 
-def get_wiki_page(
-    title="Monty Python",
-) -> str:
-    """Fetch a specific wikipedia page content
+def get_wiki_pageid(title="Monty Python") -> str:
+    """Fetch a specific wikipedia page id
 
     Args:
         title (str): The title of the page
 
     Returns:
-        str: The page content
+        str: The page id
     """
     try:
-        page = wikipedia.page(title).content
+        pageid = wikipedia.page(title).pageid
+        logging.info(f"PageId {title} returned")
+
     except wikipedia.exceptions.DisambiguationError as e:
-        page = wikipedia.page(e.options[0]).content
+        pageid = wikipedia.page(e.options[0]).pageid
         logging.exception(f"DisambiguationError: {e.options}, using {e.options[0]}")
+
     except wikipedia.exceptions.PageError as e:
         logging.exception(f"PageError: {e}")
-        page = f"{title} not found"
+        pageid = f"PageId{title} not found"
+
+    return pageid
+
+
+def get_wiki_page(
+    pageid="18942",
+) -> dict[str, str]:
+    """Fetch a specific wikipedia page content
+    using the page id
+
+    Args:
+        pageid (str): The title of the page
+
+    Returns:
+        dict[str, str]: The page id, title and text
+    """
+    try:
+        page = {'pageId': wikipedia.page(pageid=pageid).pageid, 
+                'title': wikipedia.page(pageid=pageid).title,
+                'text': wikipedia.page(pageid=pageid).content}
+        logging.info(f"Page {pageid} returned")
+
+    except wikipedia.exceptions.PageError as e:
+        logging.exception(f"PageError: {e}")
+        page = {'pageId': f"PageId{pageid} not found",
+                'title': f"PageId{pageid} not found",
+                'text': f"PageId{pageid} not found"}
 
     return page
 
 
 if __name__ == "__main__":
-    print(get_wiki_summary())
+    print(get_wiki_page())
 
 # %%
--- src/output_data.py
+++ src/output_data.py
@@ -1,3 +1,49 @@
-# output to logs
+import sqlalchemy as sq
+import logging
+import os
 
-# write to the database
+
+class Text_db:
+
+    def __init__(self, db_path):
+        self.db_path = db_path
+        self.logger = logging.getLogger('Text_db')
+        self.engine = sq.create_engine(f'sqlite:///{self.db_path}')
+
+        # does the db exist if not create it by connecting
+        if not os.path.isfile(self.db_path):
+            self.logger.info(f"New database {db_path} created")
+            # create the table
+            self.engine.execute(
+                'CREATE TABLE articles (pageId TEXT PRIMARY KEY, title TEXT NOT NULL, text TEXT NOT NULL)'
+            )
+
+        # def check if the db exists
+        def db_exists(self):
+            return os.path.isfile(self.db_path)
+
+        # if the db exists return the  current number of articles
+        def get_num_rows(self):
+            self.rows = self.engine.execute('SELECT COUNT(*) FROM articles').fetchone()[0]
+            self.logger.info(f"Database {self.db_path} has {self.rows} rows")
+            return self.rows
+
+        # insert a new article into the db
+        def insert_article(self, pageId, title, text):
+            self.engine.execute(
+                'INSERT INTO articles (pageId, title, text) VALUES (?, ?, ?)',
+                (pageId, title, text)
+            )
+
+        # get the text of an article
+        def get_article_text(self, pageId):
+            self.text = self.engine.execute(
+                'SELECT text FROM articles WHERE pageId = ?', (pageId,)
+            ).fetchone()[0]
+            return self.text
+
+        # remove an article from the db
+        def remove_article(self, pageId):
+            self.engine.execute('DELETE FROM articles WHERE pageId = ?', (pageId,))
+
+        
